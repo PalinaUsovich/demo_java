6515635f8fc5d344748c930375a00f2f358397d4
Palina <palina@Admins-MacBook-Pro.local>
2019-12-28 09:07:07 -0500

    TempHomework

---
+++ src/TempHomework.java
@@ -0,0 +1,23 @@
+public class TempHomework {
+    public static void main (String[]args){
+        //Cels to kelvin T(°C) + 273.15
+        double celsium = 30;
+        double kelvin = (celsium  + 273.15);
+
+        System.out.println(kelvin);
+        //cels to fahrenheit T(°C) × 9/5 + 32
+        double fahrenheit = celsium *5/9+ 32;
+        System.out.println(fahrenheit);
+        //Kelvin to Celsium T(K) - 273.15
+        double kelviN= 300;
+        double celsiuM= kelviN-273.15;
+        System.out.println(celsiuM);
+        //Kelvin to fahrenheit 300K × 9/5 - 459.67
+        double fahrenheiT = 300 * 9/5 - 459.67;
+        System.out.println(fahrenheiT);
+
+
+
+
+    }
+}
