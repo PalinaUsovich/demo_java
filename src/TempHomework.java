bc36343dee79e16e077970c69165d7d759f37c22
Palina <palina@admins-mbp.home>
2020-01-16 17:06:31 -0500

    TempHomework

--- src/TempHomework.java
+++ src/TempHomework.java
@@ -1,23 +1,46 @@
+import java.sql.SQLOutput;
+
 public class TempHomework {
     public static void main (String[]args){
         //Cels to kelvin T(°C) + 273.15
-        double celsium = 30;
-        double kelvin = (celsium  + 273.15);
+        double cel=getConvertCel(100);
+        if (cel<20) {
+            System.out.println("Its cold");
 
-        System.out.println(kelvin);
-        //cels to fahrenheit T(°C) × 9/5 + 32
-        double fahrenheit = celsium *5/9+ 32;
-        System.out.println(fahrenheit);
-        //Kelvin to Celsium T(K) - 273.15
-        double kelviN= 300;
-        double celsiuM= kelviN-273.15;
-        System.out.println(celsiuM);
-        //Kelvin to fahrenheit 300K × 9/5 - 459.67
-        double fahrenheiT = 300 * 9/5 - 459.67;
-        System.out.println(fahrenheiT);
+        }else{
+            System.out.println(getConvertCel(100));
+        }
+
+            System.out.println(getConvertKelvin(70));
+            System.out.println(getConvertFahr(32));
+            double newCel=getConvertedCelsium(300);
+            if( newCel<255){
+                System.out.println("It feels like spring");
+            }
+            System.out.println(getConvertKelvinToFahrenheit(456));
 
 
+       }
 
-
+    public static double getConvertCel(double fahrenheit) {
+        double celsium = (fahrenheit -32)*5/9;
+        return celsium;
     }
+    public static double getConvertKelvin (double fahrenheit){
+        double kelvin= (fahrenheit + 459.67)* 5/9;
+        return kelvin;
+    }
+    public static double getConvertFahr (double Celsium){
+        double fahrenheit =( Celsium * 9/5 + 32);
+        return fahrenheit;
+    }
+    public static double getConvertedCelsium ( double Kelvin){
+        double cels= Kelvin-273.15;
+        return cels;
+    }
+    public static double getConvertKelvinToFahrenheit (double Kelvin){
+        double newFahrenheit =Kelvin * 9/5 - 459.67;
+        return newFahrenheit;
+    }
+//      
 }
