3ac937961629cae9db2bc8c5334207c520e4b4d3
Palina <palina@Admins-MacBook-Pro.local>
2020-01-15 22:54:07 -0500

    Homework_returnMethod

--- src/Homework_returnMethod.java
+++ src/Homework_returnMethod.java
@@ -1,5 +1,5 @@
 import com.sun.codemodel.internal.JArray;
-
+import java.util.Arrays ;
 import java.util.Arrays;
 
 public class Homework_returnMethod {
@@ -10,15 +10,17 @@ public class Homework_returnMethod {
         String rev = reverse("Sample");
         System.out.println(rev);
         int[] myArray = {25, 15, 3, 2, 40};
-        System.out.println("reversed array: " + revArray(myArray));
-
-
-        //System.out.println(revArray (new int [] {25,15,3,2,40}));
-
-        int[] myNumber = {20, 10, 40, 44};
-        System.out.println("Total of numbers: " + sumArray(myNumber));
-        int[] sorting = {20, 10, 40, 44};
-       // System.out.println("Sorted array" + (sortArray(sorting)));
+        System.out.println("reversed array: " + (Arrays.toString(revArray(myArray))));
+        double[] myNumber = {20, 10, 40, 44};
+        int[] muNumber = {20, 10, 40, 44};
+        System.out.println("Total of numbers: " + sumArray(muNumber));
+        System.out.println("average is :" + aV(myNumber));
+        int[] newNumbers = {20, 10, 40, 44, 32};
+        System.out.println("the max value:" + getMaxValue(newNumbers));
+        System.out.println("the min value:"+getMinValue(newNumbers));
+        String getAbbrev ="Happy New Year ";
+       // System.out.println("Get Abbreviation :" + abbrev(getAbbrev));
+        // System.out.println("Sorted array:" + Arrays.toString(sortingArray(muNumber)));
 
 
     }
@@ -30,26 +32,23 @@ public class Homework_returnMethod {
         for (int i = Sample.length() - 1; i >= 0; i--) {
             reverse = reverse + Sample.charAt(i);
         }
-
         return reverse;
 
     }
     //2.reverse an array
-    //does not wort but i already tried everything .
 
-    public static int[] revArray(int[] numbers) {
-        int len = numbers.length;
-        int[] result = new int[len];
-        for (int i = 0; i <= len - 1; i++) {
-            result[i] = result[len - 1 - i];
 
+    public static int[] revArray(int[] num) {
+        int numLen = num.length;
+        int[] result1 = new int[numLen];
+        for (int i = 0; i <= numLen - 1; i++) {
+            result1[i] = num[numLen - 1 - i];
         }
-        return result;
+        return result1;
     }
 
     // 3.add all numbers in a given array and return 114
     public static int sumArray(int[] num) {
-        int[] nu = {20, 10, 40, 44};
         int sum = 0;
         for (int i = 0; i < num.length; i++) {
             sum = sum + num[i];
@@ -57,17 +56,59 @@ public class Homework_returnMethod {
         return sum;
     }
 
+    //    //4.sort an array (return from method 10,20,40,44)??????
+
+
+    //5.find average of given array.return 28.5
+    public static double aV(double[] myArray) {
+        double sum = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            sum = myArray[i] + sum;
+        }
+        double average;
+        average = sum / 4;
+        return average;
+    }
+
+    //6.find max number in given array
+    public static int getMaxValue(int[] max) {
+        int maxValue = max[0];
+        for (int i = 1; i < max.length; i++) {
+            if (max[i] > maxValue) {
+                maxValue = max[i];
+            }
+        }
+        return maxValue;
+    }
+    //7.find the min value in given array
+    public static int getMinValue(int []min) {
+        int minValue = min[0];
+        for (int i = 1; i < min.length; i++) {
+            if (min[i] < minValue) {
+                minValue = min[i];
+
+            }
+        }
+
+        return minValue;
+    }
+//    //8.create abbreviation for any string
+//    public static String  abbrev (String newAbbrev){
+//        String acronym="";
+//        for( int i = 0 ; {
 //
-//    //4.sort an array (return from method 10,20,40,44)
 //
 //
-//    //5.find average of given array.return 28.
-//    public static int[] average(int[]) {
 //
+//        }
+//
+//        return acronym.toUpperCase();
 //    }
-//
+
+
 
 }
+//
 
 
 
@@ -92,8 +133,9 @@ public class Homework_returnMethod {
 
 
 
-                //4.
-                //8. create abbreviation for any strings
+
+
+
 
 
 
